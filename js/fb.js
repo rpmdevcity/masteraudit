5daca047480d7ec3190b4ddca034629291f18ea4
pavel_city <pavel_city@mail.ru>
2019-11-04 10:15:12 +0300

    new

---
+++ js/fb.js
@@ -0,0 +1,23 @@
+<!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/7.2.2/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/7.2.2/firebase-analytics.js"></script>
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyBx2P9MtjamevsYQ4WxRS2K5u1a_K_3NKo",
+    authDomain: "master-audit-ae7a9.firebaseapp.com",
+    databaseURL: "https://master-audit-ae7a9.firebaseio.com",
+    projectId: "master-audit-ae7a9",
+    storageBucket: "master-audit-ae7a9.appspot.com",
+    messagingSenderId: "1042986675363",
+    appId: "1:1042986675363:web:888e500a76977d0f4ae316",
+    measurementId: "G-09L1VEBZ9H"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+</script>
